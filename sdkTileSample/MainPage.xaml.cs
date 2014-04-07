9158e8d40c08a8f75196d07342294be7cebb7953
asinta2011 <yu_li205@126.com>
2014-04-07 21:18:13 +0800

    '2014-4-8'

---
+++ sdkTileSample/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using sdkTileSample.Resources;
+
+namespace sdkTileSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void applicationTileSetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ApplicationTileSetPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+
+        private void secondaryTileSetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/SecondaryTileSetPage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
---
+++ sdkTileSample/SecondaryTileSetPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace sdkTileSample
+{
+    public partial class SecondaryTileSetPage : PhoneApplicationPage
+    {
+        public SecondaryTileSetPage()
+        {
+            InitializeComponent();
+        }
+
+        private void isSecondaryTileEnabled_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void setSecTileBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void setSecTitleBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void setSecBkBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void setSecCountBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void setSecBackTitleBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void setSecBackBkBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void setSecBackContentBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
